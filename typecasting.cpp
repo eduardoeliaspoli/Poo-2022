b64d9a5bc6f01fba78fb3c1cf1bc82800f7ceb7e
Eduardo Elias Poli <102360166+polileu@users.noreply.github.com>
2022-12-18 18:40:42 -0300

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    
+    // Declarando váriaveis inteiras;
+    int a = 7;
+    int b = 8;
+
+    // Operação base para transformar em "double", é importante utilizar x.y quando utilizar "double"
+    // nas operações;
+    double c = (7+8)/2.0;
+
+    // Imprimindo a variável c que contém as operações de type casting 
+    cout << c << endl;
+
+    return 0;
+}
